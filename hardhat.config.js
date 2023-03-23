37227103e47e93ba71c456887c753929cf234883
Максим Шуклин <maksimshuklin@192.168.1.6>
2023-03-23 21:58:56 +0300

    Added gas counter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.18",
+  gasReporter: {
+    enabled: true,
+  }
 };
+
