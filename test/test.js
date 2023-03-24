b85c065cd37060d446634a150d02bb7e64aa2b23
Максим Шуклин <maksimshuklin@192.168.1.6>
2023-03-24 19:32:47 +0300

    Added test for payoutSparse

--- test/test.js
+++ test/test.js
@@ -24,6 +24,27 @@ describe("Payout something", function () {
     expect(await payout.getBalance(user1.address)).to.equal(33);
     expect(await payout.getBalance(user2.address)).to.equal(45);
   });
+  
+  it("Payout Sparse. Small Test", async function() {
+    const Payout = await ethers.getContractFactory("PayoutContract");
+    const [owner, user1, user2] = await ethers.getSigners();
+    console.log(user1);
+    const payout = await Payout.deploy([user1.address, user2.address], 3);
+    await payout.deployed();
+
+    await payout.payoutSparse([
+      [0, 0, 1],
+      [0, 1, 2],
+      [0, 2, 3],
+      [1, 0, 2],
+      [1, 1, 3],
+      [1, 2, 4]
+    ], [1, 1, 10]);
+
+
+    expect(await payout.getBalance(user1.address)).to.equal(33);
+    expect(await payout.getBalance(user2.address)).to.equal(45);
+  });
 });
 
 /*
