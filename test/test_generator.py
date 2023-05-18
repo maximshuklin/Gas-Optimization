c01172f9289b8163bc0d4ddd15983b411caee8d2
Максим Шуклин <maksimshuklin@192.168.1.9>
2023-05-18 16:12:20 +0300

    up

--- test/test_generator.py
+++ test/test_generator.py
@@ -12,6 +12,7 @@ def generate_random(n, m, max_value = 100):
 	arr = [[random.randint(0, max_value) for col in range(m)] for row in range(n)]
 	return arr
 
+
 def generate_sparse(n, m, cap = 20, max_value = 100):
 	"""
 	Parameters:
@@ -20,7 +21,6 @@ def generate_sparse(n, m, cap = 20, max_value = 100):
 	Returns:
 		Sparse matrix size of n x m with cap percent of filled elements
 	"""
-
 	arr = [[0 for col in range(m)] for row in range(n)]
 	for it in range(n * m * cap // 100):
 		i = random.randint(0, n - 1)
@@ -44,7 +44,6 @@ def generate_low_rank(n, m, r, max_value = 100):
 		A - n x m matrix rank of r
 		U - n x r matrix, V - r x m matrix
 	"""
-
 	U = generate_random(n, r, max_value)
 	V = generate_random(r, m, max_value)
 	A = np.array(U) @ np.array(V)
@@ -60,9 +59,10 @@ def generate_repeated_columns(n, m, r, max_value = 100):
 		columns - set of columns
 		column_id - id's of columns
 	"""
-	columns = generate_random(n, r)
+	columns = generate_random(r, n)
 	column_id = [random.randint(0, r - 1) for i in range(m)]
-	return columns, column_id
+	A = np.array([columns[i] for i in column_id]).T.tolist()
+	return A
 
 
 def display_matrix(A):
